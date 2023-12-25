fdab06cf3d599a0fc08400d2556f3d7c682a4ff6
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2023-12-25 13:38:12 -0800

    Update gravitysim.js -added code to load airplane image after global variables

--- gravitysim.js
+++ gravitysim.js
@@ -25,6 +25,9 @@ const numParticles = 5000;
 const gravitationalConstant = 0.0001;
 const maxDistance = 100; // Limit calculation for performance
 
+let airplaneImage = new Image();
+airplaneImage.src = 'https://github.com/harveyspector64/Starry-Sky-Sim/blob/33a9f3026f150ac1157e6a81b61293fa6b19bf25/airplane747transparent.png'; // Your airplane image URL
+
 // Create particles with random positions and zero initial velocity
 for (let i = 0; i < numParticles; i++) {
     particles.push({
